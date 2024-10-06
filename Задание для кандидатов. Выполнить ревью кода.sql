f80ecda44cbe690219d68f8c2a6fc1dd4d998cbf
VRdcvt <139410086+VRdcvt@users.noreply.github.com>
2024-10-06 11:30:23 +0200

    Update Задание для кандидатов. Выполнить ревью кода.sql
    1. При наименовании алиаса использовать первые заглавные буквы каждого слова в названии объекта, которому дают алиас. В случае, если алиас представляет собой системное слово, добавляем первую согласную букву после заглавной из первого слова.

--- Задание для кандидатов. Выполнить ревью кода.sql
+++ Задание для кандидатов. Выполнить ревью кода.sql
@@ -9,7 +9,7 @@ begin
 -- Проверка на корректность загрузки
 	if not exists (
 	select 1
-	from syn.ImportFile as f
+	from syn.ImportFile as imf
 	where f.ID = @ID_Record
 		and f.FlagLoaded = cast(1 as bit)
 	)
